591641a9fbf282881b47ef405e1b1010f3f55f62
tongbora <tongbora.officail@gmail.com>
2025-08-24 23:10:59 +0700

    update

--- src/app/explor-program/page.tsx
+++ src/app/explor-program/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function page() {
-  return <div>This is explor program page</div>;
+  return <div className="font-h1">This is explor program page</div>;
 }
