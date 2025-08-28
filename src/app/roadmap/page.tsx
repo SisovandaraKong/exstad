b8be4defd296f7bb51fad3c933099889de05db09
KungSovannda <kungsovannda@gmail.com>
2025-08-29 00:08:50 +0700

    feat: hero section roadmap

---
+++ src/app/roadmap/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { DotPattern } from "@/components/magicui/dot-pattern";
+import BackgroundCircle from "@/components/roadmap/BackgroundCircle";
+import HorizontalScrollText from "@/components/roadmap/HorizontalScrollText";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function page() {
+  return (
+    <main>
+      {/* Hero Section */}
+      <div className="relative h-[calc(100vh-64px)] w-2/3 mx-auto flex justify-center items-center">
+        <DotPattern
+          glow
+          className="[mask-image:radial-gradient(1200px_circle_at_center,white_0%,transparent_50%)] absolute"
+        />
+        <BackgroundCircle />
+
+        <div className="absolute ">
+          <div className="flex flex-col justify-center items-center gap-5">
+            <span className="text-[64px] leading-[110%] text-center font-extrabold">
+              ROADMAP TO <br />
+              TECH MASTERY
+            </span>
+            <p className="text-[var(--text-color)]/80 text-center">
+              A structured guide to learning the right skills in the right
+              order, <br /> helping you grow into a confident developer.
+            </p>
+            <Button size={"lg"}>Explore More</Button>
+          </div>
+        </div>
+      </div>
+      <HorizontalScrollText />
+    </main>
+  );
+}
---
+++ src/components/roadmap/HorizontalScrollText.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  ScrollVelocityContainer,
+  ScrollVelocityRow,
+} from "../magicui/scroll-based-velocity";
+
+const items = [
+  "Pre-University",
+  "Foundation",
+  "Full Stack Development",
+  "Java",
+  "Dev-OPs",
+  "Spring Boot",
+  "Flutter",
+];
+
+export default function HorizontalScrollText() {
+  return (
+    <div>
+      <ScrollVelocityContainer className="text-4xl font-bold  tracking-[-0.02em]">
+        <ScrollVelocityRow baseVelocity={5} direction={1}>
+          {items.map((item) => (
+            <span className="mr-20" key={item}>
+              {item}
+            </span>
+          ))}
+        </ScrollVelocityRow>
+      </ScrollVelocityContainer>
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
+    </div>
+  );
+}
