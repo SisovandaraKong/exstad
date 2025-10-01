65150258098f30ef3b0d8a41ea5b531c79e48974
Kong_Sisovandara <sisovandara@gmail.com>
2025-10-01 12:17:04 +0700

    fixed

--- src/components/button/LogInButton.tsx
+++ src/components/button/LogInButton.tsx
@@ -3,26 +3,27 @@ import React from "react";
 import { FaUserGraduate } from "react-icons/fa";
 import { useTranslations } from "next-intl";
 import { signIn, useSession } from "next-auth/react";
+import { Button } from "../ui/button";
 
 export default function LogInButton() {
   const t = useTranslations();
   const { data } = useSession();
   if (data) {
     return (
-      <button
+      <Button
         className="flex items-center font-description-5 font-semibold text-white px-4 py-1 border bg-accent hover:bg-accent-hover rounded-full transition-colors"
       >
         <FaUserGraduate className="h-4 w-4" />
-      </button>
+      </Button>
     );
   }
   return (
-    <button
+    <Button
       onClick={() => signIn("keycloak")}
       className="flex items-center font-description-5 font-semibold text-white px-4 py-1 border bg-accent hover:bg-accent-hover rounded-full transition-colors"
     >
       <FaUserGraduate className="h-4 w-4" />
       <span className="ml-2">{t("log-in")}</span>
-    </button>
+    </Button>
   );
 }
