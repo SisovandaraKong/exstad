e3cf624a877a6079243996e8f66577dc74aab1df
Kong_Sisovandara <sisovandara@gmail.com>
2025-10-22 13:28:50 +0700

    test bakong

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -103,6 +103,7 @@ export default function AboutUsPage() {
 								<FaHandPointRight className='text-primary flex-shrink-0' />
 								Consult and connect ISTAD trainees to top IT careers
 							</p>
+							
 						</motion.div>
 						<motion.div
 							initial={{ opacity: 0, x: 20 }}
